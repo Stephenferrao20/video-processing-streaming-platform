5f0fe40363c50cb91e547acf85db8d45a6f196ea
Stephenferrao20 <stephenferrao20.in@gmail.com>
2026-01-29 11:59:14 +0530

    Changes for testing deployment 3

--- backend/src/app.js
+++ backend/src/app.js
@@ -79,7 +79,7 @@ app.use((req, res) => {
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB ', process.env.MONGODB_URI);
     
     // Start server
     const PORT = process.env.PORT || 5000;
